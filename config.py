672083ce352c6b2d5fa9103661f6d296d9933c35
sm5rt <kolaprokin0@gmail.com>
2026-01-04 01:32:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ CLUB_TAG = os.getenv("CLUB_TAG")
 
 ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID")) if os.getenv("ADMIN_USER_ID") else None
 NORM = int(os.getenv("NORM", 3000))
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DB_PATH = os.getenv("DB_PATH", "data.db")  # путь к SQLite
